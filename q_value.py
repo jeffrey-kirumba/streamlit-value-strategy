0cfe91acc82266bafe9a2abf3e29953fab41552b
jeff.k <jeff.kirumba@askari.com>
2024-07-01 07:42:53 -0500

    update Jul 2024

--- q_value.py
+++ q_value.py
@@ -8,26 +8,14 @@ import streamlit as st
 from PIL import Image
 from statistics import mean
 import base64
-
-#I know it's good practice to put keys in a secrets.py but this is a public api key
-IEX_CLOUD_API_TOKEN = 'Tpk_059b97af715d417d9f49f50b51b1c448'
-
-stocks = pd.read_csv('sp_500_stocks.csv')
-
-def build_df(portfolio_size):
-    stocks = pd.read_csv('sp_500_stocks.csv')
-    def chunks(lst, n):
-        """Yield successive n-sized chunks from lst."""
-        for i in range(0, len(lst), n):
-            yield lst[i:i + n]   
-            
-    symbol_groups = list(chunks(stocks['Ticker'], 100))
-    symbol_strings = []
-    for i in range(0, len(symbol_groups)):
-        symbol_strings.append(','.join(symbol_groups[i]))
+import yfinance as yf
+from concurrent.futures import ThreadPoolExecutor
 
 
-    rv_columns = [
+# stocks = pd.read_csv('sp_500_stocks.csv')
+class ValueScreener:
+    def __init__(self) -> None:
+        self.rv_columns = [
         'Ticker',
         'Price',
         'Number of Shares to Buy', 
@@ -42,86 +30,128 @@ def build_df(portfolio_size):
         'EV/GP',
         'EV/GP Percentile',
         'RV Score'
-    ]
+        ]
+        self.mainFrame = pd.DataFrame(columns = self.rv_columns)
+        self.tickerInfo = {}
+        pass
+    def getTickerInfo(self, ticker):
+            symbObj = yf.Ticker(ticker)
+            self.tickerInfo[ticker] = symbObj.info
 
-    rv_dataframe = pd.DataFrame(columns = rv_columns)
+    def getAllTickerInfo(self):
+        stocks = pd.read_csv('sp_500_stocks.csv')
+        allTickers = list(stocks['Ticker'])
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.getTickerInfo, allTickers)
 
-    for symbol_string in symbol_strings:
-        batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote,advanced-stats&token={IEX_CLOUD_API_TOKEN}'
-        data = requests.get(batch_api_call_url).json()
-        for symbol in symbol_string.split(','):
-            if not (data.get(symbol) is None):
-                enterprise_value = data[symbol]['advanced-stats']['enterpriseValue']
-                ebitda = data[symbol]['advanced-stats']['EBITDA']
-                gross_profit = data[symbol]['advanced-stats']['grossProfit']
-            
-                try:
-                    ev_to_ebitda = enterprise_value/ebitda
-                except TypeError:
-                    ev_to_ebitda = np.NaN
-                
-                try:
-                    ev_to_gross_profit = enterprise_value/gross_profit
-                except TypeError:
-                    ev_to_gross_profit = np.NaN
-                    
-                rv_dataframe = rv_dataframe.append(
-                    pd.Series([
-                        symbol,
-                        data[symbol]['quote']['latestPrice'],
-                        'N/A',
-                        data[symbol]['quote']['peRatio'],
-                        'N/A',
-                        data[symbol]['advanced-stats']['priceToBook'],
-                        'N/A',
-                        data[symbol]['advanced-stats']['priceToSales'],
-                        'N/A',
-                        ev_to_ebitda,
-                        'N/A',
-                        ev_to_gross_profit,
-                        'N/A',
-                        'N/A'
-                ],
-                index = rv_columns),
-                    ignore_index = True
-                )
+    def getData(self):
+        self.getAllTickerInfo()
+        for symbol in self.tickerInfo:
+            latestPrice = np.nan
+            peRatio = np.nan
+            priceToBook = np.nan
+            priceToSales = np.nan
+            enterprise_value = np.nan
+            ebitda = np.nan
+            gross_profit = np.nan
+            ev_to_ebitda = np.nan
+            ev_to_gross_profit = np.nan
+            data = self.tickerInfo[symbol]
+            if 'currentPrice' in data:
+                latestPrice = data['currentPrice']
+            elif 'regularMarketPreviousClose' in data:
+                latestPrice = data['regularMarketPreviousClose']
+            elif 'previousClose' in data:
+                latestPrice = data['previousClose']
 
-    for column in ['Price-to-Earnings Ratio', 'Price-to-Book Ratio','Price-to-Sales Ratio',  'EV/EBITDA','EV/GP']:
-        rv_dataframe[column].fillna(rv_dataframe[column].mean(), inplace = True)
+            if 'trailingPE' in data:
+                peRatio = data['trailingPE']
 
-    metrics = {
-                'Price-to-Earnings Ratio': 'PE Percentile',
-                'Price-to-Book Ratio':'PB Percentile',
-                'Price-to-Sales Ratio': 'PS Percentile',
-                'EV/EBITDA':'EV/EBITDA Percentile',
-                'EV/GP':'EV/GP Percentile'
-    }
+            if 'priceToBook' in data:
+                priceToBook = data['priceToBook']
 
-    for row in rv_dataframe.index:
-        for metric in metrics.keys():
-            rv_dataframe.loc[row, metrics[metric]] = stats.percentileofscore(rv_dataframe[metric], rv_dataframe.loc[row, metric])/100
+            if 'priceToSalesTrailing12Months' in data:
+                priceToSales = data['priceToSalesTrailing12Months']
 
-    # Print each percentile score to make sure it was calculated properly
-    for metric in metrics.values():
-        print(rv_dataframe[metric]) 
-
-    for row in rv_dataframe.index:
-        value_percentiles = []
-        for metric in metrics.keys():
-            value_percentiles.append(rv_dataframe.loc[row, metrics[metric]])
-        rv_dataframe.loc[row, 'RV Score'] = mean(value_percentiles)
-
-    rv_dataframe.sort_values(by = 'RV Score', inplace = True)
-    rv_dataframe = rv_dataframe[:50]
-    rv_dataframe.reset_index(drop = True, inplace = True)
-
-    position_size = float(portfolio_size) / len(rv_dataframe.index)
-    for i in range(0, len(rv_dataframe['Ticker'])-1):
-        rv_dataframe.loc[i, 'Number of Shares to Buy'] = math.floor(position_size / rv_dataframe['Price'][i])
+            if 'enterpriseValue' in data:
+                enterprise_value = data['enterpriseValue']
+         
+            if 'ebitda' in data:
+                ebitda = data['ebitda']
+      
+            if 'grossMargins' in data and 'totalRevenue' in data:
+                gross_profit = data['grossMargins'] * data['totalRevenue']
+        
+            if 'enterpriseToEbitda' in data:
+                ev_to_ebitda = data['enterpriseToEbitda']
+            elif ebitda:
+                ev_to_ebitda = enterprise_value/ebitda
+       
+            if enterprise_value and gross_profit:
+                ev_to_gross_profit = enterprise_value/gross_profit
     
-    rv_dataframe = rv_dataframe.replace(['N/A'], 0)
+            series = pd.Series([
+                    symbol,
+                    latestPrice,
+                    'N/A',
+                    peRatio,
+                    'N/A',
+                    priceToBook,
+                    'N/A',
+                    priceToSales,
+                    'N/A',
+                    ev_to_ebitda,
+                    'N/A',
+                    ev_to_gross_profit,
+                    'N/A',
+                    'N/A'
+            ],
+            index = self.rv_columns)
+            self.mainFrame.loc[-1] = series
+            self.mainFrame.index+=1
+            self.mainFrame.sort_index()
 
-    return rv_dataframe
+        for column in ['Price-to-Earnings Ratio', 'Price-to-Book Ratio','Price-to-Sales Ratio', 'EV/EBITDA','EV/GP']:
+            self.mainFrame[column].fillna(self.mainFrame[column].mean(), inplace = True)
+
+        metrics = {
+                    'Price-to-Earnings Ratio': 'PE Percentile',
+                    'Price-to-Book Ratio':'PB Percentile',
+                    'Price-to-Sales Ratio': 'PS Percentile',
+                    'EV/EBITDA':'EV/EBITDA Percentile',
+                    'EV/GP':'EV/GP Percentile'
+        }
+
+        for row in self.mainFrame.index:
+            for metric in metrics.keys():
+                self.mainFrame.loc[row, metrics[metric]] = stats.percentileofscore(self.mainFrame[metric], self.mainFrame.loc[row, metric])/100
+
+        # Print each percentile score to make sure it was calculated properly
+        for metric in metrics.values():
+            print(self.mainFrame[metric]) 
+
+        for row in self.mainFrame.index:
+            value_percentiles = []
+            for metric in metrics.keys():
+                value_percentiles.append(self.mainFrame.loc[row, metrics[metric]])
+            self.mainFrame.loc[row, 'RV Score'] = np.mean(value_percentiles)
+            print('value percentiles', value_percentiles)
+            print('RV Score', self.mainFrame.loc[row, 'RV Score'])
+
+    def applyPortfolioSize(self, portfolio_size):
+        mainFrame = self.mainFrame.sort_values(by = 'RV Score', ascending = False)
+        mainFrame = mainFrame[:50]
+        mainFrame.reset_index(drop = True, inplace = True)
+        position_size = float(portfolio_size) / len(mainFrame.index)
+        for i in range(0, len(mainFrame['Ticker'])-1):
+            try:
+                mainFrame.loc[i, 'Number of Shares to Buy'] = math.floor(position_size / mainFrame['Price'][i])
+            except Exception:
+                print('portfolio size', position_size)
+                print('price', mainFrame['Price'][i])
+        
+        mainFrame = mainFrame.replace(['N/A'], 0)
+        return mainFrame
 
 def filedownload(df):
     csv = df.to_csv(index=False)
@@ -129,21 +159,25 @@ def filedownload(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
     return href
 
-st.title('Robust Value Strategy')
 
+vs = ValueScreener()
+displayFrame = None
+st.title('Robust Value Strategy')
 st.write("""
 ### This investment strategy selects 50 of the cheapest stocks in the S&P 500 relative to their value (earnings and assets) """)
-
 image = Image.open('stockmarketphoto.jpg')
 st.image(image, use_column_width=True)
-
 st.write("""
-### From there, it will calculate recommended the number of shares to buy for an equal-weight portfolio of these 50 stocks.
+### From there, it will recommend the number of shares to buy for an equal-weight portfolio of these 50 stocks.
 """)
-
-capital = st.number_input('Enter the value of your portfolio')
-final_df = build_df(capital)
-st.table(final_df)
-
-st.markdown(filedownload(final_df), unsafe_allow_html=True)
-
+capital = st.number_input('Enter the value of your portfolio')      
+if 'displayFrame' not in st.session_state:
+    with st.spinner('Gathering data'):
+        vs.getData()
+        st.session_state.displayFrame = vs.mainFrame
+        displayFrame = vs.mainFrame
+elif capital > 0:
+    vs.mainFrame = st.session_state.displayFrame
+    displayFrame = vs.applyPortfolioSize(portfolio_size=capital)
+st.markdown(filedownload(displayFrame), unsafe_allow_html=True)
+st.table(displayFrame)
