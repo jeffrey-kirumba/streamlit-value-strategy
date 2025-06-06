eae3be9916d7f3ba9f1470559017752dc53181d3
jeff.k <jeff.kirumba@askari.com>
2025-06-06 17:53:07 -0500

    code clean up fixed bugs

--- q_value.py
+++ q_value.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import pandas as pd 
+import time
 import requests 
 import xlsxwriter 
 import math 
@@ -13,8 +14,42 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 # stocks = pd.read_csv('sp_500_stocks.csv')
-class ValueScreener:
+class YFWrap:
     def __init__(self) -> None:
+        self.tickerInfo = {}
+    
+    def getAllData(self):
+        stocks = pd.read_csv('sp_500_stocks.csv')
+        allTickers = list(stocks['Ticker'])
+        groups = len(allTickers) // 8
+        args = self.chunks(list(allTickers), groups)
+        try:
+            allYfTicks = []
+            for arg in args:
+                ticks = yf.Tickers(arg)
+                allYfTicks.append(ticks)
+            
+            with ThreadPoolExecutor() as executor:
+                executor.map(self.getTickerInfo, allYfTicks)
+        except Exception as e:
+            print(f"yf error: {e}")
+
+    def getTickerInfo(self, ticks: yf.Tickers):
+        try:
+            for symbol in ticks.symbols:
+                self.tickerInfo[symbol] = ticks.tickers[symbol].info
+        except Exception as e:
+            print(f"yf error: {e}")
+
+    def chunks(self, lst, n):
+        """Yield successive n-sized chunks from lst."""
+        final = []
+        for i in range(0, len(lst), n):
+            final.append(lst[i:i + n])
+        return final
+    
+class ValueScreener:
+    def __init__(self, tickerInfo) -> None:
         self.rv_columns = [
         'Ticker',
         'Price',
@@ -32,95 +67,11 @@ class ValueScreener:
         'RV Score'
         ]
         self.mainFrame = pd.DataFrame(columns = self.rv_columns)
-        self.tickerInfo = {}
+        self.tickerInfo = tickerInfo
         
-    def getTickerInfo(self, ticker):
-            try:
-                symbObj = yf.Ticker(ticker)
-                self.tickerInfo[ticker] = symbObj.info
-            except Exception as e:
-                print(e)
-                print(f'Error getting tick info for {ticker}')
-
-    def getAllTickerInfo(self):
-        stocks = pd.read_csv('sp_500_stocks.csv')
-        allTickers = list(stocks['Ticker'])
+    def calcAllTickers(self):
         with ThreadPoolExecutor() as executor:
-            executor.map(self.getTickerInfo, allTickers)
-
-    def getData(self):
-        self.getAllTickerInfo()
-        for symbol in self.tickerInfo:
-            latestPrice = np.nan
-            peRatio = np.nan
-            priceToBook = np.nan
-            priceToSales = np.nan
-            enterprise_value = np.nan
-            ebitda = np.nan
-            gross_profit = np.nan
-            ev_to_ebitda = np.nan
-            ev_to_gross_profit = np.nan
-            data = self.tickerInfo[symbol]
-            if 'currentPrice' in data and type(data['currentPrice']) != str:
-                latestPrice = data['currentPrice']
-            elif 'regularMarketPreviousClose' in data and type(data['regularMarketPreviousClose']) != str:
-                latestPrice = data['regularMarketPreviousClose']
-            elif 'previousClose' in data and type(data['previousClose']) != str:
-                latestPrice = data['previousClose']
-
-            if 'trailingPE' in data and type(data['trailingPE']) != str:
-                peRatio = data['trailingPE'] if data['trailingPE'] else np.nan
-
-            if 'priceToBook' in data and type(data['priceToBook']) != str:
-                priceToBook = data['priceToBook'] if data['priceToBook'] else np.nan
-
-            if 'priceToSalesTrailing12Months' in data and type(data['priceToSalesTrailing12Months']) != str:
-                priceToSales = data['priceToSalesTrailing12Months'] if data['priceToSalesTrailing12Months'] else np.nan
-
-            if 'enterpriseValue' in data and type(data['enterpriseValue']) != str:
-                enterprise_value = data['enterpriseValue'] if data['enterpriseValue'] else np.nan
-         
-            if 'ebitda' in data and type(data['ebitda']) != str:
-                ebitda = data['ebitda'] if data['ebitda'] else np.nan
-      
-            if 'grossMargins' in data and 'totalRevenue' in data:
-                grossMargins = data['grossMargins'] if data['grossMargins'] else np.nan
-                totalRevenue = data['totalRevenue'] if data['totalRevenue'] else np.nan
-                gross_profit = grossMargins * totalRevenue
-        
-            if 'enterpriseToEbitda' in data and type(data['enterpriseToEbitda']) != str:
-                ev_to_ebitda = data['enterpriseToEbitda'] if data['enterpriseToEbitda'] else np.nan
-            elif ebitda and enterprise_value:
-                ev_to_ebitda = enterprise_value/ebitda
-       
-            if enterprise_value and gross_profit:
-                ev_to_gross_profit = enterprise_value/gross_profit
-    
-            series = pd.Series([
-                    symbol,
-                    latestPrice,
-                    'N/A',
-                    peRatio,
-                    'N/A',
-                    priceToBook,
-                    'N/A',
-                    priceToSales,
-                    'N/A',
-                    ev_to_ebitda,
-                    'N/A',
-                    ev_to_gross_profit,
-                    'N/A',
-                    'N/A'
-            ],
-            index = self.rv_columns)
-            self.mainFrame.loc[-1] = series
-            self.mainFrame.index+=1
-            self.mainFrame.sort_index()
- 
-        for column in ['Price-to-Earnings Ratio', 'Price-to-Book Ratio','Price-to-Sales Ratio', 'EV/EBITDA','EV/GP']:
-            self.mainFrame[column].fillna(self.mainFrame[column].mean(), inplace = True)
-        
-
+                executor.map(self.calcTicker, list(self.tickerInfo.keys()))
         metrics = {
                     'Price-to-Earnings Ratio': 'PE Percentile',
                     'Price-to-Book Ratio':'PB Percentile',
@@ -128,46 +79,112 @@ class ValueScreener:
                     'EV/EBITDA':'EV/EBITDA Percentile',
                     'EV/GP':'EV/GP Percentile'
         }
+        self.mainFrame = self.mainFrame.dropna()
+        for metric in metrics.keys():
+            self.mainFrame[metrics[metric]] = self.mainFrame[metric].rank(pct=True)
 
-        for row in self.mainFrame.index:
-            for metric in metrics.keys():
-                self.mainFrame.loc[row, metrics[metric]] = stats.percentileofscore(self.mainFrame[metric], self.mainFrame.loc[row, metric])/100
+        self.mainFrame['RV Score'] = self.mainFrame[list(metrics.values())].mean(axis=1)
+        self.mainFrame = self.mainFrame.sort_values(by = 'RV Score', ascending = False)
 
-        # Print each percentile score to make sure it was calculated properly
-        for metric in metrics.values():
-            print(self.mainFrame[metric]) 
 
-        for row in self.mainFrame.index:
-            value_percentiles = []
-            for metric in metrics.keys():
-                value_percentiles.append(self.mainFrame.loc[row, metrics[metric]])
-            self.mainFrame.loc[row, 'RV Score'] = np.mean(value_percentiles)
-            print('value percentiles', value_percentiles)
-            print('RV Score', self.mainFrame.loc[row, 'RV Score'])
+    
+    def calcTicker(self, symbol):
+        latestPrice = np.nan
+        peRatio = np.nan
+        priceToBook = np.nan
+        priceToSales = np.nan
+        enterprise_value = np.nan
+        ebitda = np.nan
+        gross_profit = np.nan
+        ev_to_ebitda = np.nan
+        ev_to_gross_profit = np.nan
+        data = self.tickerInfo[symbol]
+        if 'currentPrice' in data and type(data['currentPrice']) != str:
+            latestPrice = data['currentPrice']
+        elif 'regularMarketPreviousClose' in data and type(data['regularMarketPreviousClose']) != str:
+            latestPrice = data['regularMarketPreviousClose']
+        elif 'previousClose' in data and type(data['previousClose']) != str:
+            latestPrice = data['previousClose']
+
+        if 'trailingPE' in data and type(data['trailingPE']) != str:
+            peRatio = data['trailingPE'] if data['trailingPE'] else np.nan
+
+        if 'priceToBook' in data and type(data['priceToBook']) != str:
+            priceToBook = data['priceToBook'] if data['priceToBook'] else np.nan
+
+        if 'priceToSalesTrailing12Months' in data and type(data['priceToSalesTrailing12Months']) != str:
+            priceToSales = data['priceToSalesTrailing12Months'] if data['priceToSalesTrailing12Months'] else np.nan
+
+        if 'enterpriseValue' in data and type(data['enterpriseValue']) != str:
+            enterprise_value = data['enterpriseValue'] if data['enterpriseValue'] else np.nan
+        
+        if 'ebitda' in data and type(data['ebitda']) != str:
+            ebitda = data['ebitda'] if data['ebitda'] else np.nan
+    
+        if 'grossMargins' in data and 'totalRevenue' in data:
+            grossMargins = data['grossMargins'] if data['grossMargins'] else np.nan
+            totalRevenue = data['totalRevenue'] if data['totalRevenue'] else np.nan
+            gross_profit = grossMargins * totalRevenue
+    
+        if 'enterpriseToEbitda' in data and type(data['enterpriseToEbitda']) != str:
+            ev_to_ebitda = data['enterpriseToEbitda'] if data['enterpriseToEbitda'] else np.nan
+        elif ebitda and enterprise_value:
+            ev_to_ebitda = enterprise_value/ebitda
+    
+        if enterprise_value and gross_profit:
+            ev_to_gross_profit = enterprise_value/gross_profit
+
+        series = pd.Series([
+                symbol,
+                latestPrice,
+                'N/A',
+                peRatio,
+                'N/A',
+                priceToBook,
+                'N/A',
+                priceToSales,
+                'N/A',
+                ev_to_ebitda,
+                'N/A',
+                ev_to_gross_profit,
+                'N/A',
+                'N/A'
+        ],
+        index = self.rv_columns)
+        self.mainFrame.loc[-1] = series
+        self.mainFrame.index+=1
+        self.mainFrame.sort_index()
 
     def applyPortfolioSize(self, portfolio_size):
         mainFrame = self.mainFrame.sort_values(by = 'RV Score', ascending = False)
         mainFrame = mainFrame[:50]
-        mainFrame.reset_index(drop = True, inplace = True)
         position_size = float(portfolio_size) / len(mainFrame.index)
-        for i in range(0, len(mainFrame['Ticker'])-1):
-            try:
-                mainFrame.loc[i, 'Number of Shares to Buy'] = math.floor(position_size / mainFrame['Price'][i])
-            except Exception:
-                print('portfolio size', position_size)
-                print('price', mainFrame['Price'][i])
-        
+        mainFrame['Number of Shares to Buy'] = mainFrame['Price'].rfloordiv(position_size)
         mainFrame = mainFrame.replace(['N/A'], 0)
         return mainFrame
 
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
-    return href
+def filedownload(df: pd.DataFrame):
+    if not df.empty:
+        csv = df.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+        href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+        return href
 
 
-vs = ValueScreener()
+
+
+@st.cache_resource
+def getAllData() -> ValueScreener:
+    yfRapper = YFWrap()
+    yfRapper.getAllData() 
+    vs = ValueScreener(tickerInfo=yfRapper.tickerInfo)
+    vs.tickerInfo = yfRapper.tickerInfo
+    vs.calcAllTickers()
+    return vs
+
+vs = getAllData()
+
+
 displayFrame = None
 st.title('Robust Value Strategy')
 st.write("""
@@ -177,14 +194,21 @@ st.image(image, use_column_width=True)
 st.write("""
 ### From there, it will recommend the number of shares to buy for an equal-weight portfolio of the top 50 stocks.
 """)
-capital = st.number_input('Enter the value of your portfolio')      
+capital = st.number_input('Enter the value of your portfolio')
+
 if 'displayFrame' not in st.session_state:
     with st.spinner('Gathering data'):
-        vs.getData()
         st.session_state.displayFrame = vs.mainFrame
         displayFrame = vs.mainFrame
 elif capital > 0:
     vs.mainFrame = st.session_state.displayFrame
+    # print("Applying portfolio size")
     displayFrame = vs.applyPortfolioSize(portfolio_size=capital)
+#if count < all
+    #display a warning saying ""
+if capital == 0 and displayFrame['Ticker'].count() < 290:
+    st.toast("Some tickers were left out, try again later", icon='🫡')
+
 st.markdown(filedownload(displayFrame), unsafe_allow_html=True)
+print(displayFrame.head(n=5))
 st.table(displayFrame)
